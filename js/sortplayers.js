750adabb8e44c322d09b8bb61ea74d0311a38657
Ruby Arbogast <ruby.arbogast@gmail.com>
2019-06-26 16:44:53 -0700

    Sort rosters into arrays

--- js/sortplayers.js
+++ js/sortplayers.js
@@ -1,4 +1,4 @@
-    //Initialize arrays to hold player objects -- not sure this will work
+
     //For each roster (start with just 1 to test) 
       //If position == center:
         //new object: {name = name; currentTeam = team; position = C; number = number}
@@ -10,6 +10,7 @@
       //If position == G, save to G
     //Store arrays in DB? Write to file?
 
+//Will need to change this to loops
 //Use require to load JSON object from file
 var team = require('./rosters/0.json');
 //Save the team roster to a variable 
@@ -31,8 +32,44 @@ for (let i = 0; i < teamRoster.length; i++){
             "position" : teamRoster[i]['position']['name'],
             "currentTeam" : team['teams'][0]['name']
         };
-        console.log(player);
-        //defensemen.push(teamRoster[i]);
+        defensemen.push(player);
+    } else if (teamRoster[i]['position']['name'] == 'Center') {
+        player = {
+            "name" : teamRoster[i]['person']['fullName'],
+            "number" : teamRoster[i]['jerseyNumber'],
+            "position" : teamRoster[i]['position']['name'],
+            "currentTeam" : team['teams'][0]['name']
+        };
+        centers.push(player);
+    } else if (teamRoster[i]['position']['name'] == 'Left Wing') {
+        player = {
+            "name" : teamRoster[i]['person']['fullName'],
+            "number" : teamRoster[i]['jerseyNumber'],
+            "position" : teamRoster[i]['position']['name'],
+            "currentTeam" : team['teams'][0]['name']
+        };
+        lwings.push(player);
+    } else if (teamRoster[i]['position']['name'] == 'Right Wing') {
+        player = {
+            "name" : teamRoster[i]['person']['fullName'],
+            "number" : teamRoster[i]['jerseyNumber'],
+            "position" : teamRoster[i]['position']['name'],
+            "currentTeam" : team['teams'][0]['name']
+        };
+        rwings.push(player);
+    } else if (teamRoster[i]['position']['name'] == 'Goalie') {
+        player = {
+            "name" : teamRoster[i]['person']['fullName'],
+            "number" : teamRoster[i]['jerseyNumber'],
+            "position" : teamRoster[i]['position']['name'],
+            "currentTeam" : team['teams'][0]['name']
+        };
+        goalies.push(player);
     }
-} 
-//console.log(defensemen);
+
+}
+console.log(defensemen);
+console.log(centers);
+console.log(rwings);
+console.log(lwings);
+console.log(goalies);

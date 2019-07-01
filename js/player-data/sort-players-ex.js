9f950ec64c9dd05eaa14836a23fa7d4e33fcd3ed
Ruby Arbogast <ruby.arbogast@gmail.com>
2019-06-30 22:52:29 -0700

    Remove problematic players from arrays

--- js/player-data/sort-players-ex.js
+++ js/player-data/sort-players-ex.js
@@ -1,11 +1,10 @@
-//TODO: remove P Kane, E Kane, A Watson, N Cousins, D Doughty, S Varlamov, C DeSmith -- where? 
 //TODO: change Hawks names to just "Chicago" -- where? 
 //TODO: catch errors: what if missing a file?
-//TODO: how to do this? possible ways -- write directly to DB. but then harder to change names, make deletions, etc. or store to array, make changes, then write players in separate loop -- but due to async might have to do it in a different file (or figure out promises??). this would require writing the arrays somewhere and importing them in the second file -- seems dumb. other way would require either a separate file or functions to call in the for loop -- for loop would prob add too much time to the operation to be worth it. OR change name and delete from rosters before writing (seems more efficient) -- how? 
-//TODO: may be able to skip storing files and use the API to write players directly to DB -- syntax will be more complicated but look into it
 
 //Require instance of the Player class
 const Player = require('./player-class.js');
+//Require Lodash library in order to use array methods
+const _ = require("lodash")
 
 //Require teams files and store in an array
 var allTeams = [];
@@ -27,16 +26,6 @@ var rwings = [];
 var defensemen = [];
 var goalies = [];
 
-
-var mysql = require('mysql');
-var pool  = mysql.createPool({
-  connectionLimit : 100,
-  host: "example",
-  user: "example",
-  password: "example",
-  database: "example"
-});
-
 //TODO: try wrapping in function that will return Promise, then use promise.then to write files -- ok this wouldn't actually solve anything. still would have to loop through and use connection pooling. 
 //Loop through rosters to sort players into arrays according to position 
 for (let i = 0; i < allRosters.length; i++){
@@ -48,44 +37,9 @@ for (let i = 0; i < allRosters.length; i++){
         let playerCurrentTeam = allTeams[i]['teams'][0]['name'];
 
         if (allRosters[i][j]['position']['name'] == 'Left Wing') {
-            function addRow(data) {
-                let sql = "INSERT INTO lwing (name, number, position, currentTeam) VALUES ('"+playerName+"', '"+playerNumber+"', '"+playerPosition+"', '"+playerCurrentTeam+"')";
-                
-                pool.query(sql,(err, response) => {
-                    if(err) {
-                        console.error(err);
-                        return;
-                    }
-                    // rows added
-                    console.log(response.insertId);
-                });
-            }
-
-            setTimeout(() => {
-                // call the function
-                addRow({
-                    "name": playerName,
-                    "number": playerNumber,
-                    "position": playerPosition,
-                    "currentTeam": playerCurrentTeam
-                });
-            },5000);
-
-
-/*             pool.getConnection(function(err) {
-                if (err) throw err;
-            var sql = "INSERT INTO lwings (name, number, position, currentTeam) VALUES ('"+playerName+"', '"+playerNumber+"', '"+playerPosition+"', '"+playerCurrentTeam+"')";
-            pool.query(sql, function (err, result) {
-                if (err) throw err;
-                pool.release();
-              });
-            }); */
-
-            //let currentPlayer = new Player(playerName, playerNumber, playerPosition, playerCurrentTeam);
-            //lwings.push(currentPlayer);
-        } 
-        
-/*         else if (allRosters[i][j]['position']['name'] == 'Center') {
+            let currentPlayer = new Player(playerName, playerNumber, playerPosition, playerCurrentTeam);
+            lwings.push(currentPlayer);
+        } else if (allRosters[i][j]['position']['name'] == 'Center') {
             let currentPlayer = new Player(playerName, playerNumber, playerPosition, playerCurrentTeam);
             centers.push(currentPlayer);
         } else if (allRosters[i][j]['position']['name'] == 'Right Wing') {
@@ -97,14 +51,36 @@ for (let i = 0; i < allRosters.length; i++){
         } else if (allRosters[i][j]['position']['name'] == 'Goalie') {
             let currentPlayer = new Player(playerName, playerNumber, playerPosition, playerCurrentTeam);
             goalies.push(currentPlayer);
-        } */
+        } 
     }
 
 }
 
-//Will there be problems with async again?
-/* console.log("Defenseman " . defensemen[120]); -- do this in MySQL instead? 
-console.log("Center " . centers[10]);
-console.log("Right wing " . rwings[20]);
-console.log("Left wing " . lwings[30]); */
-//console.log(goalies); 
+//Use Lodash to remove problematic players from arrays
+var probLWings = _.remove(lwings, function(p) {
+    return p.name == "Evander Kane";
+});
+
+var probCenters = _.remove(centers, function(p) {
+    return p.name == "Nick Cousins";
+});
+
+var probRWings1 = _.remove(rwings, function(p) {
+    return p.name == "Patrick Kane";
+});
+
+var probLWings2 = _.remove(lwings, function(p) {
+    return p.name == "Austin Watson";
+});
+
+var probDMen = _.remove(defensemen, function(p) {
+    return p.name == "Drew Doughty";
+});
+
+var probGoalies1 = _.remove(goalies, function(p) {
+    return p.name == "Semyon Varlamov";
+});
+
+var probGoalies2 = _.remove(goalies, function(p) {
+    return p.name == "Casey DeSmith";
+});

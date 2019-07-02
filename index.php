e67efc9b4841847f0eb4beb51cd20de26ffd918e
Ruby Arbogast <ruby.arbogast@gmail.com>
2019-07-02 14:57:45 -0700

    Update to-dos

--- index.php
+++ index.php
@@ -8,5 +8,7 @@
     <link rel="stylesheet" href="style.css">
     <?php wp_head(); ?>
   </head>
-
+  <!-- TODO: button to create team -->
+  <!-- TODO: on click, create and display team -->
+  <!-- TODO: buttons: create a new team, submit the team to blog (catch errors), save team as image -->
   </html>

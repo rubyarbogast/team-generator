2a1431b13c93d01bb821569a985ad2c9b4f577c2
Ruby Arbogast <ruby.arbogast@gmail.com>
2019-07-04 14:29:52 -0700

    Update to-dos

--- front-page.php
+++ front-page.php
@@ -1,8 +1,6 @@
 <?php get_header(); ?> 
 
 <!-- TODO: on upload, place config files in private folder -->
-<!-- TODO: button to create team -->
-<!-- TODO: on click, create and display team -->
 <!-- TODO: buttons: create a new team, submit the team to blog (catch errors), save team as image -->
 
     <section>

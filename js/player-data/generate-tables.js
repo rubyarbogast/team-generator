1d91a51d4895f6e9c0501a60c2965e6bce6924b3
Ruby Arbogast <ruby.arbogast@gmail.com>
2019-07-10 09:58:08 -0700

    Refactor MySQL; add column to tables

--- js/player-data/generate-tables.js
+++ js/player-data/generate-tables.js
@@ -9,46 +9,30 @@ var con = mysql.createConnection({
   database: config.database
 });
 
+function createTable(statement) {
+  con.query(statement, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+}
+
 //Connect to DB
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 
   //Create tables for each player position
-  var sql = "CREATE TABLE lwing (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255))";
+  var lWing = "CREATE TABLE lwing (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255), teamAbbr VARCHAR(255))";
+  var center = "CREATE TABLE center (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255), teamAbbr VARCHAR(255))";
+  var rWing = "CREATE TABLE rwing (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255), teamAbbr VARCHAR(255))";
+  var defenseman = "CREATE TABLE defenseman (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255), teamAbbr VARCHAR(255))";
+  var goalie = "CREATE TABLE goalie (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255), teamAbbr VARCHAR(255))";
 
-  var sql2 = "CREATE TABLE center (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255))";
-
-  var sql3 = "CREATE TABLE rwing (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255))";
-
-  var sql4 = "CREATE TABLE defenseman (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255))";
-
-  var sql5 = "CREATE TABLE goalie (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255))";
-
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Table lwing created");
-  });
-
-  con.query(sql2, function (err, result) {
-    if (err) throw err;
-    console.log("Table center created");
-  });
-
-  con.query(sql3, function (err, result) {
-    if (err) throw err;
-    console.log("Table rwing created");
-  });
-
-  con.query(sql4, function (err, result) {
-    if (err) throw err;
-    console.log("Table defenseman created");
-  });
-
-  con.query(sql5, function (err, result) {
-    if (err) throw err;
-    console.log("Table goalie created");
-  });
+  createTable(lWing);
+  createTable(center);
+  createTable(rWing);
+  createTable(defenseman);
+  createTable(goalie);
 });
 
 

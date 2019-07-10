096ac300c7f02cea42e9b1714851ac92631be31a
Ruby Arbogast <ruby.arbogast@gmail.com>
2019-07-10 16:12:28 -0700

    Change font and box width

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet"> 
     <title><?php wp_title('|', true, 'right'); ?><?php echo get_bloginfo('name'); ?></title> 
 
     <?php wp_head(); ?>

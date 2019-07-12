a0867294d781d77eb85790ca4734c0f12a6c3ba6
Ruby Arbogast <ruby.arbogast@gmail.com>
2019-07-12 14:08:50 -0700

    Move footer down page when JS content generated

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+</div>
             <footer id="footer">
             
                 &copy; Ruby Arbogast 2019
@@ -5,7 +6,6 @@
             </footer>
     
             <?php wp_footer(); ?>
-
-        </div>
+            
     </body>
 </html>

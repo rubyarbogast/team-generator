d9331b91bed56101e5f3ea61e1506f88b8f9406a
Ruby Arbogast <ruby.arbogast@gmail.com>
2019-07-12 12:39:51 -0700

    Hide button when clicked

--- js/site-scripts/site-scripts.js
+++ js/site-scripts/site-scripts.js
@@ -1,20 +1,28 @@
 function makeTeam() {
 
-jQuery.ajax({
-    url:  my_ajax_object.ajax_url,
-    type: "POST",
-    data: {
-        action: 'get_team',
-    },
-    dataType: "html",
-    success: function(data) {
-        // This outputs the result of the ajax request
-        document.getElementById("showTeam").innerHTML = data;
-    },
-    error: function(errorThrown){
-        console.log(errorThrown);
-}
-});
+    var x = document.getElementById("buttonDiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+
+    } else {
+        x.style.display = "none";
+    }
+
+    jQuery.ajax({
+        url:  my_ajax_object.ajax_url,
+        type: "POST",
+        data: {
+            action: 'get_team',
+        },
+        dataType: "html",
+        success: function(data) {
+            // This outputs the result of the ajax request
+            document.getElementById("showTeam").innerHTML = data;
+        },
+        error: function(errorThrown){
+            console.log(errorThrown);
+    }
+    });
 }
 /* //TODO: move DB stuff to PHP
 function makeTeam() {

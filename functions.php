73e3b9f1efb7e34ea971e37bb318005c8e349e97
Ruby Arbogast <ruby.arbogast@gmail.com>
2019-07-15 14:50:22 -0700

    Responsive JS and AJAX based on VW size

--- functions.php
+++ functions.php
@@ -1,9 +1,8 @@
 <?php
 
-//TODO: change values
 function my_enqueue() {
     wp_enqueue_script( 'makeTeam', get_template_directory_uri() . '/js/site-scripts/site-scripts.js', array('jquery') );
-    wp_localize_script( 'makeTeam', 'my_ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+    wp_localize_script( 'makeTeam', 'nhl_ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
 }
 add_action( 'wp_enqueue_scripts', 'my_enqueue' );
 
@@ -79,6 +78,7 @@ function get_team() {
         echo "0 results";
     }
 
+
     echo "<div class='team'>";
 
     echo "<h2>Forwards</h2>";
@@ -112,7 +112,7 @@ function get_team() {
 
     echo "<div class='player forward col-4'>" . $lw_result_array[3][name][0] . ". " . strstr(($lw_result_array[3][name]), ' ') . "<p>#" . $lw_result_array[3][number] . " " . $lw_result_array[3][teamAbbr] . " " . "</div>";
     echo "<div class='player forward col-4'>" . $c_result_array[3][name][0] . ". " . strstr(($c_result_array[3][name]), ' ') . "<p>#" . $c_result_array[3][number] . " " . $c_result_array[3][teamAbbr] . " " . "</div>";
-    echo "<div class='player forward col-4'>". $rw_result_array[3][name][0] . ". " . strstr(($lw_result_array[3][name]), ' ') . "<p>#" . $rw_result_array[3][number] . " " . $rw_result_array[3][teamAbbr] . "</div>";
+    echo "<div class='player forward col-4'>". $rw_result_array[3][name][0] . ". " . strstr(($rw_result_array[3][name]), ' ') . "<p>#" . $rw_result_array[3][number] . " " . $rw_result_array[3][teamAbbr] . "</div>";
     
     echo "</div>";
     echo "</div>";
@@ -160,3 +160,153 @@ function get_team() {
 
 add_action('wp_ajax_nopriv_get_team', 'get_team');
 add_action('wp_ajax_get_team', 'get_team');
+
+function get_team_desktop() {
+    $ini = parse_ini_file('config.ini');
+
+    $mysqli = new mysqli($ini['db_host'], $ini['db_user'], $ini['db_password'], $ini['db_name']);
+    if($mysqli->connect_error) {
+        exit('Could not connect');
+        }
+
+    $lwQuery = "SELECT lw.name, lw.number, lw.currentTeam, lw.position, lw.teamAbbr FROM lwing AS lw ORDER BY rand() LIMIT 4";
+    $cQuery = "SELECT c.name, c.number, c.currentTeam, c.position, c.teamAbbr FROM center AS c ORDER BY rand() LIMIT 4";
+    $rwQuery = "SELECT rw.name, rw.number, rw.currentTeam, rw.position, rw.teamAbbr FROM rwing AS rw ORDER BY rand() LIMIT 4";
+    $dQuery = "SELECT d.name, d.number, d.currentTeam, d.position, d.teamAbbr FROM defenseman AS d ORDER BY rand() LIMIT 6";
+    $gQuery = "SELECT g.name, g.number, g.currentTeam, g.position, g.teamAbbr FROM goalie AS g ORDER BY rand() LIMIT 2";
+
+    //Instantiate arrays to hold players
+    $lw_result_array = array();
+    $c_result_array = array();
+    $rw_result_array = array();
+    $d_result_array = array();
+    $g_result_array = array();
+
+    $result = mysqli_query($mysqli, $lwQuery);
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+            $lw_result_array[] = $row;
+        }
+    } else {
+        echo "0 results";
+    }
+
+    $result = mysqli_query($mysqli, $cQuery);
+    if (mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+            $c_result_array[] = $row;
+        }
+    } else {
+        echo "0 results";
+    }
+
+    $result = mysqli_query($mysqli, $rwQuery);
+    if (mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+            $rw_result_array[] = $row;
+        }
+    } else {
+        echo "0 results";
+    }
+
+    $result = mysqli_query($mysqli, $dQuery);
+    if (mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+            $d_result_array[] = $row;
+        }
+    } else {
+        echo "0 results";
+    }
+
+    $result = mysqli_query($mysqli, $gQuery);
+    if (mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+            $g_result_array[] = $row;
+        }
+    } else {
+        echo "0 results";
+    }
+
+
+    echo "<div class='team'>";
+
+    echo "<h2>Forwards</h2>";
+    echo "<div class='group'>";
+
+    echo "<div class='flex-container row'>";
+
+    echo "<div class='player forward col-4'>" . $lw_result_array[0][name] . "<p>#" . $lw_result_array[0][number] . " " . $lw_result_array[0][currentTeam] . " " . "</div>";
+    echo "<div class='player forward col-4'>" . $c_result_array[0][name] . "<p>#" . $c_result_array[0][number] . " " . $c_result_array[0][currentTeam] . " " . "</div>";
+    echo "<div class='player forward col-4'>". $rw_result_array[0][name] . "<p>#" . $rw_result_array[0][number] . " " . $rw_result_array[0][currentTeam] . "</div>";
+    
+    echo "</div>";
+
+    echo "<div class='flex-container row'>";
+
+    echo "<div class='player forward col-4'>" . $lw_result_array[1][name] . "<p>#" . $lw_result_array[1][number] . " " . $lw_result_array[1][currentTeam] . " " . "</div>";
+    echo "<div class='player forward col-4'>" . $c_result_array[1][name] . "<p>#" . $c_result_array[1][number] . " " . $c_result_array[1][currentTeam] . " " . "</div>";
+    echo "<div class='player forward col-4'>". $rw_result_array[1][name] . "<p>#" . $rw_result_array[1][number] . " " . $rw_result_array[1][currentTeam] . "</div>";
+
+    echo "</div>";
+
+    echo "<div class='flex-container row'>";
+
+    echo "<div class='player forward col-4'>" . $lw_result_array[2][name] . "<p>#" . $lw_result_array[2][number] . " " . $lw_result_array[2][currentTeam] . " " . "</div>";
+    echo "<div class='player forward col-4'>" . $c_result_array[2][name] . "<p>#" . $c_result_array[2][number] . " " . $c_result_array[2][currentTeam] . " " . "</div>";
+    echo "<div class='player forward col-4'>". $rw_result_array[2][name] . "<p>#" . $rw_result_array[2][number] . " " . $rw_result_array[2][currentTeam] . "</div>";
+
+    echo "</div>";
+
+    echo "<div class='flex-container row'>";
+
+    echo "<div class='player forward col-4'>" . $lw_result_array[3][name] . "<p>#" . $lw_result_array[3][number] . " " . $lw_result_array[3][currentTeam] . " " . "</div>";
+    echo "<div class='player forward col-4'>" . $c_result_array[3][name] . "<p>#" . $c_result_array[3][number] . " " . $c_result_array[3][currentTeam] . " " . "</div>";
+    echo "<div class='player forward col-4'>". $rw_result_array[3][name] . "<p>#" . $rw_result_array[3][number] . " " . $rw_result_array[3][currentTeam] . "</div>";
+    
+    echo "</div>";
+    echo "</div>";
+
+    echo "<h2>Defensemen</h2>";
+    echo "<div class='group'>";
+
+    echo "<div class='flex-container row'>";
+
+    echo "<div class='player dman col-6'>" . $d_result_array[0][name] . "<p>#" . $d_result_array[0][number] . " " . $d_result_array[0][currentTeam] . " ". "</div>";
+    echo "<div class='player dman col-6'>" . $d_result_array[1][name] . "<p>#" . $d_result_array[1][number] . " " . $d_result_array[1][currentTeam] . "</div>";
+
+    echo "</div>";
+
+    echo "<div class='flex-container row'>";
+
+    echo "<div class='player dman col-6'>" . $d_result_array[2][name] . "<p>#" . $d_result_array[2][number] . " " . $d_result_array[2][currentTeam] . " ". "</div>";
+    echo "<div class='player dman col-6'>" . $d_result_array[3][name] . "<p>#" . $d_result_array[3][number] . " " . $d_result_array[3][currentTeam] . "</div>";
+
+    echo "</div>";
+
+    echo "<div class='flex-container row'>";
+
+    echo "<div class='player dman col-6'>" . $d_result_array[4][name] . "<p>#" . $d_result_array[4][number] . " " . $d_result_array[4][currentTeam] . " ". "</div>";
+    echo "<div class='player dman col-6'>" . $d_result_array[5][name] . "<p>#" . $d_result_array[5][number] . " " . $d_result_array[5][currentTeam] . "</div>";
+
+    echo "</div>";
+    echo "</div>";
+
+    echo "<h2>Goalies</h2>";
+    echo "<div class='group'>";
+
+    echo "<div class='flex-container row'>";
+
+    echo "<div class='player goalie col-6'>" . $g_result_array[0][name] . "<p>#" . $g_result_array[0][number] . " " . $g_result_array[0][currentTeam] . " " . "</div>";
+    echo "<div class='player goalie col-6'>" . $g_result_array[1][name] . "<p>#" . $g_result_array[1][number] . " " . $g_result_array[1][currentTeam] . " " . "</div>";
+
+    echo "</div>";
+    echo "</div>";
+
+    echo "</div>";
+
+    wp_die(); 
+}
+
+add_action('wp_ajax_nopriv_get_team_desktop', 'get_team_desktop');
+add_action('wp_ajax_get_team_desktop', 'get_team_desktop');
--- js/site-scripts/site-scripts.js
+++ js/site-scripts/site-scripts.js
@@ -8,21 +8,47 @@ function makeTeam() {
         x.style.display = "none";
     }
 
-    jQuery.ajax({
-        url:  my_ajax_object.ajax_url,
-        type: "POST",
-        data: {
-            action: 'get_team',
-        },
-        dataType: "html",
-        success: function(data) {
-            // This outputs the result of the ajax request
-            document.getElementById("showTeam").innerHTML = data;
-        },
-        error: function(errorThrown){
-            console.log(errorThrown);
-        }
-    });
+    var windowSize = window.matchMedia("(max-width: 767px)")
+    teamDisplaySize(windowSize) // Call listener function at run time
+    windowSize.addListener(teamDisplaySize) // Attach listener function on state changes 
+
+    //NB: drawback to this approach is that it won't change if window is resized 
+    function teamDisplaySize() {
+    if (windowSize.matches) {
+        jQuery.ajax({
+            url:  nhl_ajax_object.ajax_url,
+            type: "POST",
+            data: {
+                action: 'get_team',
+            },
+            dataType: "html",
+            success: function(data) {
+                // This outputs the result of the ajax request
+                document.getElementById("showTeam").innerHTML = data;
+            },
+            error: function(errorThrown){
+                console.log(errorThrown);
+            }
+        });
+    } else {
+        jQuery.ajax({
+            url:  nhl_ajax_object.ajax_url,
+            type: "POST",
+            data: {
+                action: 'get_team_desktop',
+            },
+            dataType: "html",
+            success: function(data) {
+                // This outputs the result of the ajax request
+                document.getElementById("showTeam").innerHTML = data;
+            },
+            error: function(errorThrown){
+                console.log(errorThrown);
+            }
+        });
+    }
+    }
+
 }
 
 //TODO: prevent spam requests

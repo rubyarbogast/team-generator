ece0b3553edaa515c05b7691b2c308b186be561d
Ruby Arbogast <ruby.arbogast@gmail.com>
2019-08-23 10:42:36 -0700

    Correct syntax for retrieving player arrays

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
 <?php 
 global $wpdb;
 $team_id_list = $wpdb->get_col( "SELECT id FROM rma_team" );
-echo "Team IDs: {$team_id_list[0]}, {$team_id_list[1]}, {$team_id_list[2]}";
+//echo "Team IDs: {$team_id_list[0]}, {$team_id_list[1]}, {$team_id_list[2]}";
 ?>
 
 <?php foreach ($team_id_list as $team_id): ?>
@@ -13,21 +13,27 @@ echo "Team IDs: {$team_id_list[0]}, {$team_id_list[1]}, {$team_id_list[2]}";
 
     //Get player IDs (returns a string; has to be cast to int)
     $lw_id_array = $wpdb->get_col( "SELECT lw_id FROM rma_line WHERE team_id = $team_id" );
-    var_dump($lw_id_array);
+
 
     $lw_id1 = (int)$lw_id_array[0];
-    var_dump($lw_id1);
+    $lw_id2 = (int)$lw_id_array[1];
+    $lw_id3 = (int)$lw_id_array[2];
+    $lw_id4 = (int)$lw_id_array[3];
 
-    foreach ($lw_id_array as $lw_id) {
-    //From the players table, select the player with the id from $lines array
-        $lw = $wpdb->get_row( "SELECT * FROM rma_player WHERE id = $lw_id", ARRAY_A );
-    }
+    //From the players table, select the player with the id from the position array
+    $lw1 = $wpdb->get_row( "SELECT * FROM rma_player WHERE id = $lw_id1", ARRAY_A );
+    $lw2 = $wpdb->get_row( "SELECT * FROM rma_player WHERE id = $lw_id2", ARRAY_A );
+    $lw3 = $wpdb->get_row( "SELECT * FROM rma_player WHERE id = $lw_id3", ARRAY_A );
+    $lw4 = $wpdb->get_row( "SELECT * FROM rma_player WHERE id = $lw_id4", ARRAY_A );
 
     ?>
 
     <div class="entries">
         <p>A team</p>
         <?php echo $lw1['name'] ?>
+        <?php echo $lw2['name'] ?>
+        <?php echo $lw3['name'] ?>
+        <?php echo $lw4['name'] ?>
     </div>
 <?php endforeach; ?>
 

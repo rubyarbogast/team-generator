67291f21ce1d86e85d4903c67bbf0d06bd80ce74
Ruby Arbogast <ruby.arbogast@gmail.com>
2019-11-14 11:40:45 -0800

    Generate all tables with constraints

--- js/player-data/generate-tables.js
+++ js/player-data/generate-tables.js
@@ -21,23 +21,27 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 
-  //Create tables for each player position
-/*   var lWing = "CREATE TABLE lwing (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255), teamAbbr VARCHAR(255))";
-  var center = "CREATE TABLE center (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255), teamAbbr VARCHAR(255))";
-  var rWing = "CREATE TABLE rwing (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255), teamAbbr VARCHAR(255))";
-  var defenseman = "CREATE TABLE defenseman (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255), teamAbbr VARCHAR(255))";
-  var goalie = "CREATE TABLE goalie (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255), teamAbbr VARCHAR(255))";
-
-  createTable(lWing);
-  createTable(center);
-  createTable(rWing);
-  createTable(defenseman);
-  createTable(goalie); */
-
-  var players = "CREATE TABLE rma_all_players (nhlId INT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), currentTeam VARCHAR(255), teamAbbr VARCHAR(255), active BOOLEAN)";
+  var players = "CREATE TABLE rma_all_players (nhl_id INT PRIMARY KEY, name VARCHAR(255), number INT(2), position VARCHAR(255), current_team VARCHAR(255), team_abbr VARCHAR(255), active BOOLEAN)";
 
   createTable(players);
-});
+
+  var teams = "CREATE TABLE rma_team (team_id INT PRIMARY KEY, user INT(11), date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
+
+  createTable(teams);
+
+  var lines = "CREATE TABLE rma_line (line_id INT PRIMARY KEY, team_id INT(11), FOREIGN KEY (team_id) REFERENCES rma_team(team_id), lw_id INT(11), FOREIGN KEY (lw_id) REFERENCES rma_all_players(nhl_id), c_id INT(11), FOREIGN KEY (c_id) REFERENCES rma_all_players(nhl_id), rw_id INT(11), FOREIGN KEY (rw_id) REFERENCES rma_all_players(nhl_id))";
+
+  createTable(lines);
+
+  var pairs = "CREATE TABLE rma_pair (id INT PRIMARY KEY, team_id INT(11), FOREIGN KEY (team_id) REFERENCES rma_team(team_id), ld_id INT(11), FOREIGN KEY (ld_id) REFERENCES rma_all_players(nhl_id), rd_id INT(11), FOREIGN KEY (rd_id) REFERENCES rma_all_players(nhl_id))";
+
+  createTable(pairs);
+
+  var tandems = "CREATE TABLE rma_tandem (id INT PRIMARY KEY, team_id INT(11), FOREIGN KEY (team_id) REFERENCES rma_team(team_id), g1_id INT(11), FOREIGN KEY (g1_id) REFERENCES rma_all_players(nhl_id), g2_id INT(11), FOREIGN KEY (g2_id) REFERENCES rma_all_players(nhl_id))";
+
+  createTable(tandems);
+
+})
 
 
 
